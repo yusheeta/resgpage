18667f258f8f92481d3222ed15d8b8e4ebf9d6d9
yusheeta <43299850+yusheeta@users.noreply.github.com>
2021-06-01 16:16:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
-const connection = require('./config/database');
-
-// Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
-
-// Need to require the entire Passport config module so app.js knows about it
-require('./config/passport');
+const cors = require('cors');
+const path = require('path');
+const passport = require('passport');
 
 /**
  * -------------- GENERAL SETUP ----------------
@@ -22,30 +13,36 @@ require('dotenv').config();
 // Create the Express application
 var app = express();
 
+// Configures the database and opens a global connection that can be used in any module with `mongoose.connection`
+require('./config/database');
+
+// Must first load the models
+require('./models/user');
+
+// Pass the global passport object into the configuration function
+require('./config/passport')(passport);
+
+// This will initialize the passport object on every request
+app.use(passport.initialize());
+
+// Instead of using body-parser middleware, use the new Express implementation of the same thing
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// Allows our Angular application to make HTTP requests to Express application
+app.use(cors());
 
-/**
- * -------------- SESSION SETUP ----------------
- */
-
-// TODO
-
-/**
- * -------------- PASSPORT AUTHENTICATION ----------------
- */
-
-app.use(passport.initialize());
-app.use(passport.session());
-
+// Where Angular builds to - In the ./angular/angular.json file, you will find this configuration
+// at the property: projects.angular.architect.build.options.outputPath
+// When you run `ng build`, the output will go to the ./public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 /**
  * -------------- ROUTES ----------------
  */
 
 // Imports all of the routes from ./routes/index.js
-app.use(routes);
+app.use(require('./routes'));
 
 
 /**
